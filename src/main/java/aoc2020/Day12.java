96a7c09c35c7c0047707e31185a42092a403e0f0
Paul Cormier <Paul.E.Cormier@rcmp-grc.gc.ca>
2020-12-12 12:11:36 -0500

    Day 12 - part 2
    That was fun. It might be cute to render the steps of some of these.

--- src/main/java/aoc2020/Day12.java
+++ src/main/java/aoc2020/Day12.java
@@ -60,8 +60,108 @@ public class Day12 {
     }
 
     /**
+     * Navigate the ship to its final position, using a waypoint, and determine the
+     * Manhattan Distance from the origin.
      */
     private static void part2() {
+        Ship ferry = new Ship();
+        Waypoint waypoint = new Waypoint();
+
+        List<String> instructions = FileUtils.readFile(INPUT_TXT);
+        for (String instruction : instructions) {
+
+            int value = Integer.parseInt(instruction.substring(1));
+            char instructionChar = instruction.charAt(0);
+
+            switch (instructionChar) {
+                case LEFT:
+                    waypoint.turn(-value);
+                    break;
+                case RIGHT:
+                    waypoint.turn(value);
+                    break;
+                case FORWARD:
+                    ferry.advanceTowardWaypoint(waypoint, value);
+                    break;
+                // Must be a cardinal direction
+                default:
+                    waypoint.translate(CardinalDirection.valueOf(String.valueOf(instructionChar)), value);
+                    break;
+            }
+        }
+
+        System.out.println("The ship's final position is: " + ferry);
+        System.out.println("The Manhattan Distance is: " + (Math.abs(ferry.positionEW) + Math.abs(ferry.positionNS)));
+    }
+
+    /**
+     * Model the waypoint's position relative to the ship.
+     */
+    private static class Waypoint {
+        int positionEW = 10;
+        int positionNS = 1;
+
+        /**
+         * Translate the waypoint in the given direction by the distance given.
+         * 
+         * @param direction The {@link CardinalDirection} in which to translate the
+         *     waypoint.
+         * @param distance The distance to move the waypoint.
+         */
+        public void translate(CardinalDirection direction, int distance) {
+            switch (direction) {
+                case N:
+                    positionNS += distance;
+                    break;
+                case S:
+                    positionNS -= distance;
+                    break;
+                case E:
+                    positionEW += distance;
+                    break;
+                case W:
+                    positionEW -= distance;
+                    break;
+            }
+        }
+
+        /**
+         * Rotate the waypoint around the ship by a number of degrees.
+         * 
+         * @param degrees A multiple of 90 degrees by which to rotate the ship. A
+         *     positive value means to the right, a negative value means to the left.
+         */
+        public void turn(int degrees) {
+
+            int absoluteDegrees = (degrees + 360) % 360;
+            int temp;
+            switch (absoluteDegrees) {
+                case 90:
+                    // NS = -EW, EW = NS
+                    temp = -positionEW;
+                    positionEW = positionNS;
+                    positionNS = temp;
+                    break;
+                case 180:
+                    // NS = -NS, EW = -EW
+                    positionEW = -positionEW;
+                    positionNS = -positionNS;
+                    break;
+                case 270:
+                    // NS = EW, EW = -NS
+                    temp = positionEW;
+                    positionEW = -positionNS;
+                    positionNS = temp;
+                    break;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s %d, %s %d",
+                                 positionEW >= 0 ? "east" : "west", Math.abs(positionEW),
+                                 positionNS >= 0 ? "north" : "south", Math.abs(positionNS));
+        }
     }
 
     /**
@@ -73,7 +173,7 @@ public class Day12 {
         int positionNS = 0;
 
         /**
-         * Rotate the ship's orientation be a number of degrees.
+         * Rotate the ship's orientation by a number of degrees.
          * 
          * @param degrees A multiple of 90 degrees by which to rotate the ship. A
          *     positive value means to the right, a negative value means to the left.
@@ -92,6 +192,20 @@ public class Day12 {
          */
         public void advance(int distance) {
             translate(orientation, distance);
+        }
+
+        /**
+         * Translate the ship in the direction of the waypoint by the given number of
+         * times.
+         * 
+         * @param heading The direction, and distance, to move the ship.
+         * @param times The number of times to move the ship towards the waypoint.
+         */
+        public void advanceTowardWaypoint(Waypoint heading, int times) {
+            // Determine the total EW, and NS, distance to translate
+            translate(CardinalDirection.E, heading.positionEW * times);
+            translate(CardinalDirection.N, heading.positionNS * times);
+
         }
 
         /**
