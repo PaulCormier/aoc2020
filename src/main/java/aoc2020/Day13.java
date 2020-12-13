f22b3f1a894ca0ddcea7f6d4fd05d900ae2d2ad6
Paul Cormier <Paul.E.Cormier@rcmp-grc.gc.ca>
2020-12-13 01:56:25 -0500

    Day 13 - part 2
    Honestly, I don't understand this at all. It could be because it's
    almost 2am...
    
    Something, something, Chinese Remainder Theorem...

--- src/main/java/aoc2020/Day13.java
+++ src/main/java/aoc2020/Day13.java
@@ -1,5 +1,6 @@
 package aoc2020;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -40,11 +41,118 @@ public class Day13 {
         int waitTime = nextBus - (int) (timestamp) % nextBus;
         System.out.println("The nex bus is #" + nextBus + " in " + waitTime + " minutes.");
         System.out.println("Solution: " + waitTime * nextBus);
+
     }
 
     /**
      * Find the time when the busses depart in a particular order.
      */
     private static void part2() {
+        List<String> lines = FileUtils.readFile(INPUT_TXT);
+
+        // The second line is all the running bus numbers.
+        String[] busRoutes = lines.get(1).split(",");
+
+        System.out.println(Arrays.toString(busRoutes));
+
+        // Chinese remainder theorem?
+        // https://www.freecodecamp.org/news/how-to-implement-the-chinese-remainder-theorem-in-java-db88a3f1ffe0/
+        int[] numbers = Stream.of(busRoutes)
+                              .filter(StringUtils::isNumeric)
+                              .mapToInt(Integer::parseInt)
+                              .toArray();
+
+        long[] remainders = new long[numbers.length];
+        int j = 0;
+        for (int i = 0; i < busRoutes.length; i++) {
+            if (StringUtils.isNumeric(busRoutes[i]))
+                remainders[j++] = (numbers[j - 1] - i) % numbers[j - 1];
+        }
+
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(remainders));
+
+        // Step 1: Find the product of all the numbers in the first array.
+        long product = 1;
+        for (int i = 0; i < numbers.length; i++) {
+            product *= numbers[i];
+        }
+
+        // Step 2: Find the partial product of each number.
+        long[] partialProduct = new long[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            partialProduct[i] = product / numbers[i];
+        }
+
+        // 3. Find the modular multiplicative inverse of number[i] modulo
+        // partialProduct[i].
+        long[] inverse = new long[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            inverse[i] = computeInverse(partialProduct[i], numbers[i]);
+        }
+
+        // Step 4: Final Sum
+        long sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += partialProduct[i] * inverse[i] * remainders[i];
+        }
+
+        System.out.println("Final number: " + sum % product);
+
+    }
+
+    public static long computeInverse(long a, long b) {
+        long m = b, t, q;
+        long x = 0, y = 1;
+        if (b == 1)
+            return 0;
+        // Apply extended Euclid Algorithm
+        while (a > 1) {
+            // q is quotient
+            q = a / b;
+            t = b;
+            // now proceed same as Euclid's algorithm
+            b = a % b;
+            a = t;
+            t = x;
+            x = y - q * x;
+            y = t;
+        } // Make x1 positive
+        if (y < 0)
+            y += m;
+        return y;
+    }
+
+    /**
+     * Find the time when the busses depart in a particular order.
+     */
+    private static void part2_slow() {
+        List<String> lines = FileUtils.readFile(INPUT_TXT);
+
+        // The second line is all the running bus numbers.
+        String[] busRoutes = lines.get(1).split(",");
+
+        System.out.println(Arrays.toString(busRoutes));
+
+        long numberOfRoutes = Stream.of(busRoutes)
+                                    .filter(StringUtils::isNumeric)
+                                    .count();
+
+        // Just brute force it
+        long t = 1;
+        int n = 0;
+        while (t < Long.MAX_VALUE && n < numberOfRoutes) {
+            n = 0;
+            for (int i = 0; i < busRoutes.length; i++) {
+                if (StringUtils.isNumeric(busRoutes[i])) {
+                    if ((t + i) % Integer.parseInt(busRoutes[i]) != 0)
+                        break;
+                    n++;
+                }
+            }
+            t++;
+        }
+
+        System.out.println("The starting time is: " + t);
     }
 }
