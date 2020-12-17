d7ca3bcc23ee266e675f1e9a03498511a316a904
Paul Cormier <Paul.E.Cormier@rcmp-grc.gc.ca>
2020-12-17 12:02:10 -0500

    Day 17 - part 1 (Parsing the file)
    Just reading the test input, and printing the initial grid.

---
+++ src/main/java/aoc2020/Day17.java
@@ -0,0 +1,212 @@
+package aoc2020;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Conway Cubes
+ * 
+ * https://adventofcode.com/2020/day/17
+ *
+ * @author Paul Cormier
+ * 
+ */
+public class Day17 {
+
+    private static final char ACTIVE_CUBE = '#';
+    private static final char INACTIVE_CUBE = '.';
+
+    private static final String INPUT_TXT = "Input-Day17.txt";
+    private static final String TEST_INPUT_TXT = "TestInput-Day17.txt";
+
+    public static void main(String[] args) {
+        part1();
+        part2();
+    }
+
+    /**
+     * How many active cubes are there after the 6th cycle?
+     */
+    private static void part1() {
+        // Initialise the Conway space
+        List<char[]> lines = FileUtils.readFileToStream(TEST_INPUT_TXT)
+                                      .map(String::toCharArray)
+                                      .collect(Collectors.toList());
+
+        // Create cubes for each known input.
+        Map<Coordinates, Cube> knownCubes = new HashMap<>();
+
+        int maxY = (int) Math.ceil((lines.size() - 1) / 2.);
+        int maxX = maxY;
+        int y = 0;
+        int z = 0;
+        for (char[] line : lines) {
+            maxX = (int) Math.ceil((line.length - 1) / 2.);
+            for (int x = 0; x < line.length; x++) {
+                Cube cube = new Cube(Coordinates.of(x - maxX, y - maxY, z), line[x] == ACTIVE_CUBE);
+
+                // Link the known neighbours above, and to the left
+                //                for (int i = 0; i >= -1; i--) {
+                //                    for (int j = 0; j >= -1; j--) {
+                //                        Optional.ofNullable(knownCubes.get(Coordinates.of(x - maxX + i, y - maxY + j, z)))
+                //                                .ifPresent(c -> c.addNeighbour(cube));
+                //                    }
+                //                }
+                knownCubes.put(cube.getCoordinates(), cube);
+            }
+            y++;
+        }
+
+        //        System.out.println(knownCubes);
+
+        printCubes(knownCubes, maxX, maxY, 0);
+    }
+
+    /**
+     */
+    private static void part2() {
+    }
+
+    /**
+     * Print out the contents of a map of cubes in a grid, with bounds: +/- maxX
+     * by +/- maxY by +/- maxZ.
+     * 
+     * @param cubes
+     *            The map of cubes to be printed.
+     * @param maxX
+     *            The number of cubes wide to be printed away from the centre.
+     * @param maxY
+     *            The number of cubes high to be printed away from the centre.
+     * @param maxZ
+     *            The number of layers of cubes to be printed away from the
+     *            centre.
+     */
+    private static void printCubes(Map<Coordinates, Cube> cubes, int maxX, int maxY, int maxZ) {
+        char[][][] grid = new char[maxZ * 2 + 1][maxY * 2 + 1][maxX * 2 + 1];
+
+        for (Cube cube : cubes.values()) {
+            Coordinates coordinates = cube.getCoordinates();
+            int x = coordinates.x + maxX;
+            int y = coordinates.y + maxY;
+            int z = coordinates.z + maxZ;
+            grid[z][y][x] = cube.isActive() ? ACTIVE_CUBE : INACTIVE_CUBE;
+        }
+
+        // Print the grid
+        for (int z = 0; z < grid.length; z++) {
+            System.out.println("z=" + (z - maxZ));
+            for (char[] row : grid[z]) {
+                System.out.println(String.valueOf(row));
+            }
+            System.out.println();
+        }
+
+    }
+
+    /**
+     * A Conway cell in three-dimensional space.
+     */
+    private static class Cube {
+        private boolean active = false;
+        private final Coordinates coordinates;
+        private final Set<Cube> neighbours;
+
+        public Cube(Coordinates coordinates, boolean active) {
+            this.active = active;
+            this.coordinates = coordinates;
+            this.neighbours = new HashSet<>();
+        }
+
+        public boolean isActive() {
+            return active;
+        }
+
+        public void setActive(boolean active) {
+            this.active = active;
+        }
+
+        public Coordinates getCoordinates() {
+            return coordinates;
+        }
+
+        public Set<Cube> getNeighbours() {
+            return neighbours;
+        }
+
+        /**
+         * Link two cubes as neighbours.
+         * 
+         * @param neighbour
+         *            The neighbour to associate with this cube.
+         */
+        public void addNeighbour(Cube neighbour) {
+            this.neighbours.add(neighbour);
+            neighbour.neighbours.add(this);
+        }
+
+        public int getActiveNeighbours() {
+            return (int) neighbours.stream().filter(Cube::isActive).count();
+        }
+
+        @Override
+        public String toString() {
+            return coordinates + " " + (active ? "Active" : "Inactive");
+        }
+
+    }
+
+    private static class Coordinates {
+        private final int x;
+        private final int y;
+        private final int z;
+
+        private Coordinates(int x, int y, int z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static Coordinates of(int x, int y, int z) {
+            return new Coordinates(x, y, z);
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + x;
+            result = prime * result + y;
+            result = prime * result + z;
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Coordinates other = (Coordinates) obj;
+            if (x != other.x)
+                return false;
+            if (y != other.y)
+                return false;
+            if (z != other.z)
+                return false;
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("(%d,%d,%d)", x, y, z);
+        }
+
+    }
+}
