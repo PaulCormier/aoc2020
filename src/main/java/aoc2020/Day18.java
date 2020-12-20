9df4f0c80a8ea65b17dfa64e1a9d7211a60683eb
Paul Cormier <Paul.E.Cormier@rcmp-grc.gc.ca>
2020-12-20 00:24:01 -0500

    Day 18 - part 2 (wip)
    So close with adding parentheses... But I think I need to try a LHS +
    RHS approach.

--- src/main/java/aoc2020/Day18.java
+++ src/main/java/aoc2020/Day18.java
@@ -17,7 +17,7 @@ public class Day18 {
     private static final String TEST_INPUT_2_TXT = "TestInput2-Day18.txt";
 
     public static void main(String[] args) {
-        part1();
+        // part1();
         part2();
     }
 
@@ -25,36 +25,48 @@ public class Day18 {
      * What is the sum of the resulting values?
      */
     private static void part1() {
-        
+
         // Get the questions
         // Parse the questions
         // Find the sum of the solutions
         long sum = FileUtils.readFileToStream(INPUT_TXT)
-                           // .peek(q -> System.out.print(q + " = "))
-                           .mapToLong(Day18::parseString)
-                           // .peek(System.out::println)
-                           .sum();
+                            // .peek(q -> System.out.print(q + " = "))
+                            .mapToLong(Day18::parseString)
+                            // .peek(System.out::println)
+                            .sum();
         System.out.println("The sum is: " + sum);
     }
 
     /**
+     * What is the sum of the resulting values?
      */
     private static void part2() {
+
+        // Get the questions
+        // Parse the questions
+        // Find the sum of the solutions
+        long sum = FileUtils.readFileToStream(TEST_INPUT_TXT)
+                            .peek(System.out::println)
+                            // .peek(q -> System.out.print(q + " = "))
+                            .map(Day18::addParentheses)
+                            .mapToLong(Day18::parseString)
+                            .sum();
+        System.out.println("The sum is: " + sum);
     }
 
     /**
-     * Parse a string containing an arithmetic expression. Brackets are evaluated
-     * first, then operators are evaluated in left-to-right order.
+     * Add parentheses around the addition operations to enforce operator order.
      * 
-     * @param line The arithmetic expression to be evaluated.
-     * @return The numerical result of the expression.
+     * @param line The arithmetic expression.
+     * @return The new expression, with parentheses around the addition operations.
      */
-    private static long parseString(String line) {
-        long total = 0;
-        long value;
-        char operator = '+';
+    private static String addParentheses(String line) {
+        StringBuilder output = new StringBuilder();
 
-        char[] characters = line.trim().toCharArray();
+        int parensAdded = 0;
+        String value = null;
+
+        char[] characters = line.toCharArray();
         for (int i = 0; i < characters.length; i++) {
             // Check for parentheses
             if (characters[i] == '(') {
@@ -68,7 +80,81 @@ public class Day18 {
                             break;
                     }
                 }
-                value = parseString(" "+new String(characters, i + 1, j - i - 1));
+                output.append("(")
+                      .append(addParentheses(new String(characters, i + 1, j - i - 1)))
+                      .append(")");
+                if (parensAdded > 0) {
+                    output.append(')');
+                    parensAdded--;
+                }
+                value = null;
+                i = j;
+            }
+
+            // Number
+            if (NumberUtils.isCreatable(String.valueOf(characters[i]))) {
+                value = String.valueOf(characters[i]);
+            }
+
+            // Operator
+            if (characters[i] == '*') {
+                if (value != null)
+                    output.append(value);
+                if (parensAdded > 0) {
+                    output.append(')');
+                    parensAdded--;
+                }
+                output.append('*');
+            }
+            if (characters[i] == '+') {
+                
+                if (value != null) {
+                        output.append('(').append(value);
+                    parensAdded++;
+                }
+                output.append('+');
+            }
+        }
+        if (value != null) {
+            output.append(value);
+        }
+        if (parensAdded > 0) {
+            output.append(')');
+            parensAdded--;
+        }
+
+        return output.toString();
+    }
+
+    /**
+     * Parse a string containing an arithmetic expression. Brackets are evaluated
+     * first, then operators are evaluated in left-to-right order.
+     * 
+     * @param line The arithmetic expression to be evaluated.
+     * @return The numerical result of the expression.
+     */
+    private static long parseString(String line) {
+        //
+
+        long total = 0;
+        long value;
+        char operator = '+';
+
+        char[] characters = line.toCharArray();
+        for (int i = 0; i < characters.length; i++) {
+            // Check for parentheses
+            if (characters[i] == '(') {
+                // Find the end, evaluate the substring, and set the resulting value
+                int j = i;
+                for (int parens = 1; j < characters.length; j++) {
+                    if (characters[j] == '(')
+                        parens++;
+                    else if (characters[j] == ')') {
+                        if (--parens == 1)
+                            break;
+                    }
+                }
+                value = parseString(" " + new String(characters, i + 1, j - i - 1));
                 i = j;
                 total = operator == '+' ? total + value : total * value;
             }
@@ -86,4 +172,5 @@ public class Day18 {
         System.out.println(line + " = " + total);
         return total;
     }
+
 }
