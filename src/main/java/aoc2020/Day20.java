8da332d5c2bcc7acb5b7b8a4892cfe1de408e0c2
Paul Cormier <Paul.E.Cormier@rcmp-grc.gc.ca>
2020-12-22 12:21:32 -0500

    Day 20 - part 2 (wip)
    Getting there... Had a ~very~ buggy rotate method. Now to fix the
    replacement of the sea monsters.

--- src/main/java/aoc2020/Day20.java
+++ src/main/java/aoc2020/Day20.java
@@ -25,6 +25,7 @@ public class Day20 {
 
     private static final String PUZZLE_INPUT_TXT = "Input-Day20.txt";
     private static final String TEST_PUZZLE_INPUT_TXT = "TestInput-Day20.txt";
+    private static final String TEST_2_PUZZLE_INPUT_TXT = "TestInput2-Day20.txt";
 
     private static final String TEST_TILE = "Tile 1951:\n" +
                                             "#.##...##.\n" +
@@ -37,10 +38,21 @@ public class Day20 {
                                             ".###....#.\n" +
                                             "..#.#..#.#\n" +
                                             "#...##.#..";
+    private static final String TEST_TILE_2 = "Tile 3527:\n" +
+                                              "........##\n" +
+                                              ".........#\n" +
+                                              "........#.\n" +
+                                              "...#..##.#\n" +
+                                              "......##..\n" +
+                                              "###.#..#.#\n" +
+                                              "...#....#.\n" +
+                                              "#.......##\n" +
+                                              "..........\n" +
+                                              ".#....###.";
 
     public static void main(String[] args) {
         //        part1();
-        part2();
+                part2();
         //        Tile testTile = new Tile(TEST_TILE);
         //        for (int i = 1; i <= 4; i++) {
         //            System.out.println(testTile);
@@ -51,6 +63,19 @@ public class Day20 {
         //            System.out.println(testTile);
         //            testTile.rotate(1);
         //        }
+
+//        Tile testTile2 = new Tile(TEST_TILE_2);
+//        testTile2.flipVertical();
+//        System.out.println(testTile2.tileToString());
+//        System.out.println();
+//        System.out.println(testTile2);
+//        System.out.println();
+//        testTile2.rotate(3);
+//        System.out.println(testTile2.tileToString());
+//        System.out.println();
+//        System.out.println(testTile2);
+//        System.out.println();
+
     }
 
     /**
@@ -89,7 +114,7 @@ public class Day20 {
         Map<Integer, List<Tile>> sideMap = new HashMap<>();
 
         // Parse the input
-        List<Tile> tiles = FileUtils.cleanInput(FileUtils.readFileToStream(PUZZLE_INPUT_TXT))
+        List<Tile> tiles = FileUtils.cleanInput(FileUtils.readFileToStream(TEST_2_PUZZLE_INPUT_TXT))
                                     .map(Tile::new)
                                     //                                    .peek(System.out::println)
                                     .peek(t -> {
@@ -111,7 +136,7 @@ public class Day20 {
                            .get();
         arrangement[0][0] = corner;
 
-        //        corner.flipVertical();
+        corner.flipVertical();
         //        System.out.println("Top-left corner: \n" + corner);
 
         // Orient it correctly
@@ -138,7 +163,7 @@ public class Day20 {
         //        System.out.println("Top-left corner: \n" + corner);
         int row = 0;
         do {
-            // Fill in the first row
+            // Fill in the row
             for (int i = 1; i < size; i++) {
                 // Find the tile which goes to the right
                 Tile leftTile = arrangement[row][i - 1];
@@ -151,6 +176,7 @@ public class Day20 {
                     rightTile.flipVertical();
                 }
                 rightTile.rotate(3 + rightTile.getSidesFlipped().indexOf(matchingSide));
+
             }
 
             //        System.out.println(ArrayUtils.toString(arrangement[0]));
@@ -177,6 +203,20 @@ public class Day20 {
                 mapRows[i] = StringUtils.defaultString(mapRows[i]) + new String(arrangement[i / 8][j].getData()[i % 8]);
         }
 
+        // Show the tiles for troubleshooting
+        String[] tileRows = new String[size * 10];
+        for (int i = 0; i < size * 10; i++) {
+            for (int j = 0; j < size; j++)
+                tileRows[i] = StringUtils.defaultString(tileRows[i]) + new String(arrangement[i / 10][j].getFullData()[i % 10]);
+        }
+
+        int rowNum = 0;
+        for (String tileRow : tileRows) {
+            if (rowNum++ % 10 == 0) {
+                System.out.println(Stream.of(arrangement[rowNum / 10]).map(t -> t.getId() + ":      ").collect(Collectors.joining()));
+            }
+            System.out.println(tileRow.replaceAll("(.{10})", "$1 "));
+        }
         // Look for sea monsters
         int foundSeaMonsters = 0;
         Pattern body1 = Pattern.compile("#(....)##(....)##(....)###");
@@ -184,14 +224,14 @@ public class Day20 {
         String bodyReplace1 = "O$1OO$2OO$3OOO";
         String bodyReplace2 = "$1O$2O$3O$4O$5O$6O";
 
-//        long startingHashes = Stream.of(mapRows).collect(Collectors.joining("\n")).chars().filter(c -> c == '#').count();
-//
-//        System.out.println("There are " + startingHashes + " \"#\"s to start with.");
-        
-//        ArrayUtils.reverse(mapRows);
-//        mapRows = rotate(mapRows);
-//        mapRows = rotate(mapRows);
-//        mapRows = rotate(mapRows);
+        //        long startingHashes = Stream.of(mapRows).collect(Collectors.joining("\n")).chars().filter(c -> c == '#').count();
+        //
+        //        System.out.println("There are " + startingHashes + " \"#\"s to start with.");
+
+        //        ArrayUtils.reverse(mapRows);
+        //        mapRows = rotate(mapRows);
+        //        mapRows = rotate(mapRows);
+        //        mapRows = rotate(mapRows);
         search: for (int x = 0; x < 2; x++) {
             for (int y = 0; y < 4; y++) {
                 for (int i = 1; i < mapRows.length - 1; i++) {
@@ -199,12 +239,13 @@ public class Day20 {
                     Matcher row1Matcher = body1.matcher(mapRows[i]);
                     Matcher row2Matcher = body2.matcher(mapRows[i + 1]);
                     while (row1Matcher.find()) {
+                        int row1Start = row1Matcher.start();
                         while (row2Matcher.find()) {
-                            if (row1Matcher.start() == row2Matcher.start() &&
-                                mapRows[i - 1].charAt(row1Matcher.start() + 18) == '#') {
+                            int row2Start = row2Matcher.start();
+                            if (row1Start == row2Start && mapRows[i - 1].charAt(row1Start + 18) == '#') {
                                 foundSeaMonsters++;
-                                mapRows[i - 1] = mapRows[i - 1].substring(0, row1Matcher.start() + 18) + "O" +
-                                                 mapRows[i - 1].substring(row1Matcher.start() + 19);
+                                mapRows[i - 1] = mapRows[i - 1].substring(0, row1Start + 18) + "O" +
+                                                 mapRows[i - 1].substring(row1Start + 19);
                                 StringBuffer replacement = new StringBuffer();
                                 mapRows[i] = row1Matcher.appendReplacement(replacement, bodyReplace1)
                                                         .appendTail(replacement).toString();
@@ -214,19 +255,23 @@ public class Day20 {
                                                             .appendTail(replacement).toString();
                                 //                                mapRows[i + 1] = row2Matcher.replaceAll(bodyReplace2);
                             }
+                            row2Matcher.region(row2Start + 1, mapRows[i].length());
                         }
                         row2Matcher.reset();
+                        row1Matcher.region(row1Start + 1, mapRows[i].length());
                     }
                 }
 
-//                if (foundSeaMonsters > 0)
-//                    break search;
+                if (foundSeaMonsters > 0)
+                    break search;
 
                 // Rotate and try again
                 mapRows = rotate(mapRows);
+                tileRows = rotate(tileRows);
             }
             // Flip the map and try again
             ArrayUtils.reverse(mapRows);
+            ArrayUtils.reverse(tileRows);
         }
         System.out.println("Found sea monsters: " + foundSeaMonsters);
         String map = Stream.of(mapRows).collect(Collectors.joining("\n"));
@@ -236,6 +281,15 @@ public class Day20 {
         long remainingHashes = map.chars().filter(c -> c == '#').count();
 
         System.out.println("There are " + remainingHashes + " remaining \"#\"s.");
+
+        //         rowNum = 0;
+        //        for (String tileRow : tileRows) {
+        //            if (rowNum++ % 10 == 0) {
+        //                System.out.println(Stream.of(arrangement[rowNum / 10]).map(t->t.getId()+":      ").collect(Collectors.joining()));
+        //            }
+        //            System.out.println(tileRow.replaceAll("(.{10})", "$1 "));
+        //        }
+        System.out.println(Stream.of(tileRows).collect(Collectors.joining("\n")));
     }
 
     /**
@@ -264,6 +318,7 @@ public class Day20 {
         private List<Integer> sidesFlipped;
 
         private char[][] data;
+        private char[][] fullData;
         private final int id;
 
         /**
@@ -311,6 +366,12 @@ public class Day20 {
                               .map(String::toCharArray)
                               .collect(Collectors.toList())
                               .toArray(new char[0][0]);
+
+            // Keep the full data for troubleshooting.
+            this.fullData = Stream.of(content.split("\n"))
+                                  .map(String::toCharArray)
+                                  .collect(Collectors.toList())
+                                  .toArray(new char[0][0]);
         }
 
         public int getId() {
@@ -319,6 +380,10 @@ public class Day20 {
 
         public char[][] getData() {
             return data;
+        }
+
+        public char[][] getFullData() {
+            return fullData;
         }
 
         public List<Integer> getSides() {
@@ -339,21 +404,33 @@ public class Day20 {
             Collections.rotate(sides, times);
             Collections.rotate(sidesFlipped, -times);
 
-            if (times % 4 == 2) {
-                Stream.of(data).forEach(ArrayUtils::reverse);
-                ArrayUtils.reverse(data);
-            } else if (times % 2 == 1) {
+//            if (times % 4 == 2) {
+//                Stream.of(data).forEach(ArrayUtils::reverse);
+//                ArrayUtils.reverse(data);
+//                Stream.of(fullData).forEach(ArrayUtils::reverse);
+//                ArrayUtils.reverse(fullData);
+//            } else if (times % 2 == 1) {
                 int size = data.length;
-                char[][] ret = new char[size][size];
 
-                // Rotate the matrix 1, or 3 times.
+                // Rotate the matrix.
                 for (int time = 0; time < times % 4; time++) {
+                    char[][] ret = new char[size][size];
                     for (int i = 0; i < size; ++i)
                         for (int j = 0; j < size; ++j)
                             ret[i][j] = data[size - j - 1][i];
                     data = ret;
                 }
-            }
+                int size2 = fullData.length;
+
+                // Rotate the matrix.
+                for (int time = 0; time < times % 4; time++) {
+                    char[][] ret2 = new char[size2][size2];
+                    for (int i = 0; i < size2; ++i)
+                        for (int j = 0; j < size2; ++j)
+                            ret2[i][j] = fullData[size2 - j - 1][i];
+                    fullData = ret2;
+                }
+//            }
         }
 
         /**
@@ -366,6 +443,10 @@ public class Day20 {
 
             // Reverse each line
             for (char[] row : data) {
+                ArrayUtils.reverse(row);
+            }
+            // Reverse each line
+            for (char[] row : fullData) {
                 ArrayUtils.reverse(row);
             }
         }
@@ -385,6 +466,10 @@ public class Day20 {
         public String toString() {
             String dataString = Stream.of(data).map(String::new).collect(Collectors.joining("\n"));
             return "Tile " + this.id + ":\n" + dataString + "\n" + sides + "\n" + sidesFlipped + "\n";
+        }
+
+        public String tileToString() {
+            return Stream.of(fullData).map(String::new).collect(Collectors.joining("\n"));
         }
     }
 }
